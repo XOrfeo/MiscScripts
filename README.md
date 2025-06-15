f66cdd59557a98bd97533182f644317c87ac5805
XOrfeo <dmgardner13@gmail.com>
2025-06-15 19:02:05 +0100

    Added Anagram Solver & day Finder Added the anagram solver & dayFinder scripts added config setup Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Misc Scripts
+Miscellaneous Python Scripts
+
+## First Time setup
+
+run command 
+```bash
+bash init
+```
+
+This sets up the execution permissions for the config script which allows you easily modify the config file used by each script.
+
+## Config
+
+The config script can be used to modify the config file (conf.conf). It provides an alternative to manually editing the file, although that does remain an option.
+
+To use the script, execute the command
+
+```bash
+./config [options]
+```
+
+To see a list of available options, execute the command with either no options or the -h option.
+
+## Usage
+### Anagram Solver
+
+This script finds all perfect anagrams of the input word. It loads the wordfile configured in the config file (conf.conf) and asks you for the initial word. It then returns all perfect anagrams (same length & same letters) of the input word.
+
+Execute the script using the command
+```bash
+python3 anagram.py
+```
+
+### Day of week Finder
+
+This script returns the day of the week that a given day is. It asks for a date in ISO format (YYYY-MM-DD).
+
+Execute the script using the command
+```bash
+python3 dayFinder.py
+```
+It is based on the information at this site: https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html 
---
+++ dayFinder.py
@@ -0,0 +1,50 @@
+import sys
+from datetime import date as dat
+
+def isLeapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def validDate(day,month,year):
+    monthDays = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:81,9:30,10:31,11:30,12:31}
+
+    if month < 1 or month > 12:
+        return False
+
+    if day < 1:
+        return False
+    
+    if isLeapYear(year):
+        if day -1 > monthDays[month]:
+            return False
+    else:
+        if day > monthDays[month]:
+            return False
+        
+    return True
+        
+date = dat.fromisoformat(input("Please enter date in ISO format YYYY-MM-DD => "))
+
+if not validDate(date.day,date.month,date.year):
+    print("Invalid Date")
+    sys.exit()
+
+monthKey = {1:1,2:4,3:4,4:0,5:2,6:5,7:0,8:3,9:6,10:1,11:4,12:6}
+
+DOWList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+centuryOffset = {1:4,2:2,3:0,0:6}
+
+counter = int((date.year%100)/4) + date.day + monthKey[date.month] + centuryOffset[int(date.year/100)%4] + (date.year%100)
+
+offset = counter % 7
+
+print (date,"will be a",DOWList[offset-1])
